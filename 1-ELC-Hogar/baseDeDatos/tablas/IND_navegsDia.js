e2694ef2c0a0b713a9955fb46ab06576c9d1fa94
diribar <diegoiribarren2015@gmail.com>
2024-11-15 07:42:48 -0300

    Cambios de nombre

--- 1-ELC-Hogar/baseDeDatos/tablas/IND_navegsDia.js
+++ 1-ELC-Hogar/baseDeDatos/tablas/IND_navegsDia.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, dt) => {
-	const alias = "rutasDelDia";
+	const alias = "navegsDia";// navegaciones por rutas durante el día
 	const columns = {
 		cliente_id: {type: dt.STRING(11)},
 		ruta: {type: dt.STRING(100)},
 		fecha: {type: dt.DATE},
 	};
 	const config = {
-		tableName: "ind_rutas_del_dia",
+		tableName: "ind_navegs_dia",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
