ef8ea0062a891517df9ecf72a7c8dd42b4b79e53
diribar <diegoiribarren2015@gmail.com>
2024-11-20 15:52:07 -0300

    Mejora de procesos

--- 1-ELC-Hogar/baseDeDatos/tablas/IND_navegsDiaHoraCant.js
+++ 1-ELC-Hogar/baseDeDatos/tablas/IND_navegsDiaHoraCant.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, dt) => {
 	const alias = "navegsDiaHoraCant"; // cantidad de navegantes por hora de día de la semana
 	const columns = {
+		fecha: {type: dt.DATE},
 		diaSem: {type: dt.STRING(3)},
 		hora: {type: dt.INTEGER},
-		semAnt: {type: dt.INTEGER},
-		semAct: {type: dt.INTEGER},
+		cant: {type: dt.INTEGER},
 	};
 	const config = {
 		tableName: "ind_navegs_dia_hora_cant",
