bd97f772653a63dfb11393544366abe79d5f0c74
diribar <diegoiribarren2015@gmail.com>
2024-11-21 15:26:13 -0300

    Mejora continua

--- 1-ELC-Hogar/publico/javascript/6-Graficos/GR-navegsPorHora.js
+++ 1-ELC-Hogar/publico/javascript/6-Graficos/GR-navegsPorHora.js
@@ -3,6 +3,8 @@ window.addEventListener("load", async () => {
 	// Obtiene datos del BE
 	const registros = await fetch(ruta).then((n) => n.json());
 
+	// Variables
+	const DOM = {grafico: document.querySelector("#zonaDeGraficos #cuadro #grafico")};
 
 	// Fin
 	return;
