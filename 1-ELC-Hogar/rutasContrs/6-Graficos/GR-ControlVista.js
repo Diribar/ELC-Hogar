7aec60b748572fe6bcf6ae615dc5a394e0ef511a
diribar <diegoiribarren2015@gmail.com>
2024-11-21 14:24:11 -0300

    Mejora de procesos

--- 1-ELC-Hogar/rutasContrs/6-Graficos/GR-ControlVista.js
+++ 1-ELC-Hogar/rutasContrs/6-Graficos/GR-ControlVista.js
@@ -4,8 +4,7 @@ const exportar = {};
 for (let codigo in graficos)
 	exportar[codigo] = (req, res) => {
 		res.render("CMP-0Estructura", {
-			tema: "gráficos",
-			codigo,
+			...{tema: "gráficos", codigo},
 			titulo: graficos[codigo].titulo,
 		});
 	};
