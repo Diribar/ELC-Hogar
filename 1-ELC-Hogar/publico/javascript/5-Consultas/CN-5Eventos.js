c949d4f9b4b9b8b34b83370740f2db4a9494e062
diribar <diegoiribarren2015@gmail.com>
2024-12-05 05:59:36 -0300

    Mejora de procesos

--- 1-ELC-Hogar/publico/javascript/5-Consultas/CN-5Eventos.js
+++ 1-ELC-Hogar/publico/javascript/5-Consultas/CN-5Eventos.js
@@ -326,24 +326,22 @@ window.addEventListener("load", async () => {
 
 		// De lo contrario, alterna entre lo que se muestra sobre la 'imagen derecha'
 		else if (elemento == DOM.imgDerecha) {
-			let seHicieronCambios;
-
 			// 1. Si se ve 'vistaDeResults', lo oculta y muestra 'cartelRCLV'
 			if (!DOM.vistaDeResults.className.includes("toggle")) {
 				DOM.vistaDeResults.classList.add("toggle");
 				if (DOM.cartelRCLV) DOM.cartelRCLV.classList.remove("toggle");
-				seHicieronCambios = true;
 			}
 
 			// 2. Si se ve 'cartelRCLV', lo oculta
-			if (!seHicieronCambios && DOM.cartelRCLV && !DOM.cartelRCLV.className.includes("toggle")) {
+			else if (!seHicieronCambios && DOM.cartelRCLV && !DOM.cartelRCLV.className.includes("toggle")) {
 				DOM.vistaDeResults.classList.add("toggle");
-				DOM.cartelRCLV.classList.add("toggle");
-				seHicieronCambios = true;
+				if (DOM.cartelRCLV) DOM.cartelRCLV.classList.add("toggle");
 			}
 
 			// 3. Si no se hicieron cambios, muestra 'vistaDeResults'
-			if (!seHicieronCambios) DOM.vistaDeResults.classList.remove("toggle");
+			else DOM.vistaDeResults.classList.remove("toggle");
 		}
+		console.log(elemento, padre);
+		console.log(nombre, padre.id);
 	});
 });
