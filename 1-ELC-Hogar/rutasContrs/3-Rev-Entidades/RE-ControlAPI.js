b1708abb1c89a7fe681115d99ac8ef8504e866a1
diribar <diegoiribarren2015@gmail.com>
2024-12-06 17:44:49 -0300

    Mejora continua

--- 1-ELC-Hogar/rutasContrs/3-Rev-Entidades/RE-ControlAPI.js
+++ 1-ELC-Hogar/rutasContrs/3-Rev-Entidades/RE-ControlAPI.js
@@ -5,7 +5,7 @@ const procsProd = require("../2.1-Prods-RUD/PR-FN-Procesos");
 const procesos = require("./RE-Procesos");
 
 module.exports = {
-	// Productos y RCLV
+	// Productos y RCLVs
 	obtieneMotivoGenerico: (req, res) => res.json(motivoInfoErronea.id),
 	edicAprobRech: async (req, res) => {
 		// Variables
@@ -76,6 +76,54 @@ module.exports = {
 	},
 
 	// Links
+	aprobInactivo: async (req, res) => {
+		// Variables
+		const {url} = req.query;
+		const link = await baseDeDatos.obtienePorCondicion("links", {url}, variables.entidades.asocsProd);
+
+		// Más variables
+		const datos = procesos.variables({link, req});
+		const {id, statusRegistro_id, statusCreado, decisAprob, datosLink, campoDecision} = datos;
+		const {motivo_id, revId, statusOriginalPor_id, statusOriginal_id, statusFinal_id} = datos;
+
+		// CONSECUENCIAS - Actualiza el registro del link
+		await baseDeDatos.actualizaPorId("links", id, datosLink);
+
+		// CONSECUENCIAS - Acciones si no es un 'creadoAprob' convertido en 'aprobado'
+		if (statusOriginal_id != creadoAprob_id || statusFinal_id != aprobado_id) {
+			// Agrega un registro en el statusHistorial
+			let datosHist = {
+				entidad_id: id,
+				entidad: "links",
+				statusOriginal_id: link.statusRegistro_id,
+				statusFinal_id: statusRegistro_id,
+				statusOriginalPor_id,
+				statusFinalPor_id: revId,
+				statusOriginalEn: statusCreado ? link.creadoEn : link.statusSugeridoEn,
+				aprobado: decisAprob,
+			};
+			let motivo;
+			if (motivo_id) {
+				motivo = statusMotivos.find((n) => n.id == motivo_id);
+				datosHist.motivo_id = motivo_id;
+				datosHist.duracion = Number(motivo.duracion);
+				datosHist.comentario = motivo.descripcion;
+			}
+			baseDeDatos.agregaRegistro("statusHistorial", datosHist);
+
+			// Aumenta el valor de linksAprob/rech en el registro del usuario
+			baseDeDatos.aumentaElValorDeUnCampo("usuarios", statusOriginalPor_id, campoDecision, 1);
+
+			// Penaliza al usuario si corresponde
+			if (motivo) comp.penalizacAcum(statusOriginalPor_id, motivo, "links");
+		}
+
+		// CONSECUENCIAS - Actualiza los productos en los campos de 'links'
+		await procsFM.accsEnDepsPorCambioDeStatus("links", {...link, statusRegistro_id});
+
+		// Fin
+		return res.json("");
+	},
 	sigProdLinks: async (req, res) => {
 		// Variables
 		const {entidad, id} = req.query;
