69cbe080459306f9605824fb70508fbd6114beaa
diribar <diegoiribarren2015@gmail.com>
2024-12-09 00:06:01 -0300

    Mejora continua

--- 1-ELC-Hogar/publico/javascript/2.0-Familias/FM-PRL-CorregirStatus.js
+++ 1-ELC-Hogar/publico/javascript/2.0-Familias/FM-PRL-CorregirStatus.js
@@ -3,13 +3,12 @@
 window.addEventListener("load", async () => {
 	// Variables
 	const DOM = {
-		inputs: document.querySelectorAll("#historial input[name='opcion']"),
 		form: document.querySelector("form"),
 		submit: document.querySelector("form button[type='submit']"),
 	};
 
-	// Eventos - Input
-	for (let input of DOM.inputs) input.addEventListener("change", () => submit.classList.remove("inactivo"));
+	// Eventos - Change
+	DOM.form.addEventListener("change", () => DOM.submit.classList.remove("inactivo"));
 
 	// Eventos - Submit
 	DOM.form.addEventListener("submit", (e) => {
