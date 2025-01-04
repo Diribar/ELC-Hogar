6e4829903fb0135ee33d44434fb7378708ea263e
diribar <diegoiribarren2015@gmail.com>
2025-01-04 15:50:34 -0300

    Miscelaneas

--- 1-ELC-Hogar/middlewares/porUsuario/urlAgregarProd.js
+++ 1-ELC-Hogar/middlewares/porUsuario/urlAgregarProd.js
@@ -4,13 +4,9 @@ module.exports = (req, res, next) => {
 	// Variables
 	const {cliente_id} = req.session.cliente;
 	let {originalUrl: ruta} = req;
-	let comentario;
-
-	// Motivos para discontinuar la función
-	if (cliente_id == "U000000001") return next();
 
 	// Guarda el registro de navegación
-	comp.guardaRegistroNavegac({cliente_id, ruta, comentario, reqHeaders: req.headers["user-agent"]});
+	comp.guardaRegistroNavegac({cliente_id, ruta, comentario: null, reqHeaders: req.headers["user-agent"]});
 
 	// Fin
 	next();
